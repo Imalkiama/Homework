93aa481afcc2e07c25fa46df7745d9f3e9079001
Imalkiama <85057789+Imalkiama@users.noreply.github.com>
2021-06-04 22:41:59 +0530

    Add files via upload

---
+++ AreaOfTriangle.java
@@ -0,0 +1,7 @@
+class AreaOfTriangle  {  
+   public static void main (String[]args) {         
+   float b=12,h =06,area ;  
+	//area = ( b*h) / 2 ;//
+    System.out.println("Area of Triangle is: "+area);  
+    }
+}  
