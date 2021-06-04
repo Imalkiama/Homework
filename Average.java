7c632195a1cd8bd0140368e0166ebf9f171202bc
Imalkiama <85057789+Imalkiama@users.noreply.github.com>
2021-06-04 22:40:50 +0530

    Add files via upload

---
+++ Average.java
@@ -0,0 +1,12 @@
+class Average {
+    public static void main(String[]args) {
+        int a=1;
+		int b=2;
+		int c=3;
+		int d=4;
+		int e=5;
+		int average;
+		/*average=a+b+c+d+e;*/
+	System.out.println(average); 
+    }
+}
